5f54589decd93b659ea85f52eb5448d287c58800
maliha-afk <24amaliha@gmail.com>
2024-09-29 21:30:12 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+my_set={1,2,3,4,5,6,7,8,9}
+new_set={1,2,3,4,5}
+dup_set={1,2,1,2,1,2,1,2}
+
+my_set.add(10)
+print(my_set)
+
+#remove
+#discard
+
+#remove
+my_set.remove(6)
+print(my_set)
+
+#discard
+my_set.discard(34)
+print(my_set)
+
+#membership
+print(9 in my_set)
